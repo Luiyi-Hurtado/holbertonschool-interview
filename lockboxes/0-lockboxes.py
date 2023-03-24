a874163b361929181ea330cf1437e29dbb7685d0
Luiyi-Hurtado <luiyifernando@gmail.com>
2023-03-24 19:34:02 +0000

    [TASK-0]: create script that determinates if all the boxes can be opened

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    """
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    stack = [0]
+
+    while stack:
+        box = stack.pop()
+        for key in boxes[box]:
+            if key >= 0 and key < n and not visited[key]:
+                visited[key] = True
+                stack.append(key)
+
+    return all(visited)
