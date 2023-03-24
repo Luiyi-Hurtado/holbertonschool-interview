3ca8f908ca970a9a604d949275f183e6356247d0
Luiyi-Hurtado <luiyifernando@gmail.com>
2023-03-24 19:45:58 +0000

    [TASK-0]: add docstrings in this module

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
 """
+Fucntion that determiantes if all the boxes can be opened
 """
 
 
 def canUnlockAll(boxes):
     """
+    Return True if the boxes is opened or
+    False if the boxes is not opened
+
+    Args:
+        boxes (list):
+            a list with the boxes
+
+    Return:
+        Boolean :
+            a boolean that indicate if the boxes
+            is opened or not
     """
     n = len(boxes)
     visited = [False] * n
