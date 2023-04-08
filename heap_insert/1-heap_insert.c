ccb43153faaa738b54307822fb9ddd132e851985
Luiyi-Hurtado <luiyifernando5@gmail.com>
2023-04-08 16:50:43 -0500

    [TASK-1]: create the correct function

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -1,99 +1,55 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "binary_trees.h"
 
-/* Original code from http://stackoverflow.com/a/13755911/5184480 */
-
 /**
- * print_t - Stores recursively each level in an array of strings
- *
- * @tree: Pointer to the node to print
- * @offset: Offset to print
- * @depth: Depth of the node
- * @s: Buffer
- *
- * Return: length of printed tree after process
+ * length - find the length of the binary tree.
+ * @root: the root node.
+ * Return: the length of the binary tree.
  */
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s)
+int length(heap_t **root)
 {
-	char b[6];
-	int width, left, right, is_left, i;
-
-	if (!tree)
+	if (!root || !*root)
 		return (0);
-	is_left = (tree->parent && tree->parent->left == tree);
-	width = sprintf(b, "(%03d)", tree->n);
-	left = print_t(tree->left, offset, depth + 1, s);
-	right = print_t(tree->right, offset + left + width, depth + 1, s);
-	for (i = 0; i < width; i++)
-		s[depth][offset + left + i] = b[i];
-	if (depth && is_left)
-	{
-		for (i = 0; i < width + right; i++)
-			s[depth - 1][offset + left + width / 2 + i] = '-';
-		s[depth - 1][offset + left + width / 2] = '.';
-	}
-	else if (depth && !is_left)
-	{
-		for (i = 0; i < left + width; i++)
-			s[depth - 1][offset - width / 2 + i] = '-';
-		s[depth - 1][offset + left + width / 2] = '.';
-	}
-	return (left + width + right);
+
+	return (length(&((*root)->left)) + length(&((*root)->right)) + 1);
 }
 
-/**
- * _height - Measures the height of a binary tree
- *
- * @tree: Pointer to the node to measures the height
- *
- * Return: The height of the tree starting at @node
- */
-static size_t _height(const binary_tree_t *tree)
-{
-	size_t height_l;
-	size_t height_r;
-
-	height_l = tree->left ? 1 + _height(tree->left) : 0;
-	height_r = tree->right ? 1 + _height(tree->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
-}
 
 /**
- * binary_tree_print - Prints a binary tree
- *
- * @tree: Pointer to the root node of the tree to print
+ * heap_insert - insert a node into the max heap.
+ * @root: the root node.
+ * @value: the value to insert.
+ * Return: the node inserted or null.
  */
-void binary_tree_print(const binary_tree_t *tree)
+heap_t *heap_insert(heap_t **root, int value)
 {
-	char **s;
-	size_t height, i, j;
+	heap_t *node = NULL;
+	int len = (length(root) - 1) / 2;
 
-	if (!tree)
-		return;
-	height = _height(tree);
-	s = malloc(sizeof(*s) * (height + 1));
-	if (!s)
-		return;
-	for (i = 0; i < height + 1; i++)
+	if (!root)
+		return (NULL);
+
+	node = malloc(sizeof(heap_t));
+
+	if (!node)
+		return (NULL);
+
+	node = binary_tree_node(*root, value);
+
+	if (!*root)
 	{
-		s[i] = malloc(sizeof(**s) * 255);
-		if (!s[i])
-			return;
-		memset(s[i], 32, 255);
+		*root = node;
+		return (node);
 	}
-	print_t(tree, 0, 0, s);
-	for (i = 0; i < height + 1; i++)
-	{
-		for (j = 254; j > 1; --j)
-		{
-			if (s[i][j] != ' ')
-				break;
-			s[i][j] = '\0';
-		}
-		printf("%s\n", s[i]);
-		free(s[i]);
-	}
-	free(s);
+	if (len == 0)
+		return (NULL);
+	if (len > 0 && len % 2)
+		(*root) = (*root)->left;
+	if (len > 0 && !(len % 2))
+		*root = (*root)->right;
+	node->(*root) = *root;
+	if (!(*root)->left)
+		(*root)->left = node;
+	else
+		(*root)->right = node;
+	return (node);
 }
